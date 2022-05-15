14dc8b51615452db15eb074b561b4cd0e4456a0a
Eddie Shin <eshinhw@gmail.com>
2022-05-14 21:41:33 -0400

    Initial commit

---
+++ app/src/main/java/com/eddieshin/emojiworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eddieshin.emojiworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
