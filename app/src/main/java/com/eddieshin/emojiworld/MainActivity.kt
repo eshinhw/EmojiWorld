49eefd6155a4f26657b385a31ce9b1f60f4abef0
Eddie Shin <eshinhw@gmail.com>
2022-05-15 02:39:57 -0400

    finished the tutorials for the project

--- app/src/main/java/com/eddieshin/emojiworld/MainActivity.kt
+++ app/src/main/java/com/eddieshin/emojiworld/MainActivity.kt
@@ -1,19 +1,41 @@
 package com.eddieshin.emojiworld
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spanned
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
+data class User (
+    val displayName: String = "",
+    val emojis: String = ""
+)
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "MainActivity"
     }
+
+    private val db = Firebase.firestore
+
 
     private lateinit var auth: FirebaseAuth
 
@@ -22,6 +44,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         auth = Firebase.auth
+
+        val query = db.collection("users")
+        val options = FirestoreRecyclerOptions.Builder<User>().setQuery(query, User::class.java)
+            .setLifecycleOwner(this).build()
+
+        val adapter = object: FirestoreRecyclerAdapter<User, UserViewHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+                val view = LayoutInflater.from(this@MainActivity).inflate(android.R.layout.simple_list_item_2, parent, false)
+                return UserViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: User) {
+                val tvName: TextView = holder.itemView.findViewById(android.R.id.text1)
+                val tvEmojis: TextView = holder.itemView.findViewById(android.R.id.text2)
+
+                tvName.text = model.displayName
+                tvEmojis.text = model.emojis
+
+            }
+
+        }
+
+        rvUsers.layoutManager = LinearLayoutManager(this)
+        rvUsers.adapter = adapter
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -39,6 +87,79 @@ class MainActivity : AppCompatActivity() {
             startActivity(logoutIntent)
         }
 
+        if (item.itemId == R.id.miEditStatus) {
+            Log.i(TAG, "Show Alert Dialog")
+            showAlertDialog()
+
+
+        }
+
         return super.onOptionsItemSelected(item)
+    }
+
+    inner class EmojiFilter : InputFilter {
+        override fun filter(
+            source: CharSequence?,
+            start: Int,
+            end: Int,
+            dest: Spanned?,
+            dstart: Int,
+            dend: Int
+        ): CharSequence {
+            // if the added text is valid, return source
+            // if it's invalid, then ""
+            if (source == null || source.isBlank()) {
+                return ""
+            }
+            Log.i(TAG, "Added text $source has length ${source.length} characters")
+
+            val validCharTypes = listOf(Character.SURROGATE, Character.NON_SPACING_MARK, Character.OTHER_SYMBOL).map{
+                it.toInt()
+            }
+            for (inputChar in source) {
+                val type = Character.getType(inputChar)
+                Log.i(TAG, "Character type $type")
+                if (!validCharTypes.contains(type)) {
+                    // Toast message
+                    return ""
+                }
+            }
+            return source
+        }
+
+    }
+
+    private fun showAlertDialog() {
+        val editText = EditText(this)
+
+        val lengthFilter = InputFilter.LengthFilter(9)
+        val emojiFilter = EmojiFilter()
+        editText.filters = arrayOf(lengthFilter)
+
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Update your emoji status")
+            .setView(editText)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show()
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            Log.i(TAG, "Clicked on positive buttion!")
+            val emojisEntered = editText.text.toString()
+            if (emojisEntered.isBlank()) {
+                Toast.makeText(this, "Cannot submit empty text!", Toast.LENGTH_LONG)
+                return@setOnClickListener
+            }
+            val currentUser = auth.currentUser
+            if (currentUser == null) {
+                Toast.makeText(this, "No signed in user!", Toast.LENGTH_LONG)
+                return@setOnClickListener
+            }
+
+            // update firestore with new emojis
+
+            db.collection("users").document(currentUser.uid)
+                .update("emojis", emojisEntered)
+            dialog.dismiss()
+        }
     }
 }
